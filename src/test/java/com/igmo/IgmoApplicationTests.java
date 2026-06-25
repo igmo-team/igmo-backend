8819744a3f4f0bcadf42dffbdaad032174e3d447
Lee Dong Yeop <s01054830455@gmail.com>
2026-06-25 17:42:06 +0900

    test: 사용하지 않는 테스트 제거

--- src/test/java/com/igmo/IgmoApplicationTests.java
+++ src/test/java/com/igmo/IgmoApplicationTests.java
@@ -1,24 +1,12 @@
 package com.igmo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class IgmoApplicationTests {
 
-    @Value("${spring.application.name}")
-    private String applicationName;
-
     @Test
     void contextLoads() {
     }
-
-    @Test
-    void applicationNameIsIgmo() {
-        assertThat(applicationName).isEqualTo("igmo");
-    }
-
 }
