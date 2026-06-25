3ebb5db1d0afcc55109a9e233fa2dfe455c26cb1
Lee Dong Yeop <s01054830455@gmail.com>
2026-06-25 17:40:47 +0900

    test: ci 확인용 테스트 추가 (#3)
    * test: ci 확인용 테스트 추가
    
    * chore: ci 실패 레포트 생성 job 추가

--- src/test/java/com/igmo/IgmoApplicationTests.java
+++ src/test/java/com/igmo/IgmoApplicationTests.java
@@ -1,13 +1,24 @@
 package com.igmo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class IgmoApplicationTests {
 
+    @Value("${spring.application.name}")
+    private String applicationName;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void applicationNameIsIgmo() {
+        assertThat(applicationName).isEqualTo("igmo");
     }
 
 }
