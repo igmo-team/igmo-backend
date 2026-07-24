513471cff1c3c8593d12fdcc4112b7f83bb44edd
Lee Dong Yeop <s01054830455@gmail.com>
2026-07-25 00:39:37 +0900

    test: ArchUnit 의존성 제거
    - ArchUnit JUnit 5 테스트 의존성 제거\n- ArchUnit 전용 관리자 의존성 규칙 테스트 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,6 @@ dependencies {
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("com.tngtech.archunit:archunit-junit5:1.4.1")
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
     testImplementation("com.epages:restdocs-api-spec-mockmvc:0.19.4")
     testCompileOnly("org.projectlombok:lombok")
